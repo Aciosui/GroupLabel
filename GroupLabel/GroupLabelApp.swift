88db982440b84022fa6da9bd23d71e1af242eb51
aciosui <mrana.swe@gmail.com>
2024-03-27 19:05:08 -0400

    Initial Commit

---
+++ GroupLabel/GroupLabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupLabelApp.swift
+//  GroupLabel
+//
+//  Created by MD  on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GroupLabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
